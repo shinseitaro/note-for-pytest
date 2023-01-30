a4a7de5e35459063150b97e864a272b9b71c2e67
shinseitaro <shinsei.all@gmail.com>
2023-01-30 10:38:39 +0900

    fortest event

---
+++ tests/test_sample.py
@@ -0,0 +1,17 @@
+from connpass_client import ConnpassClient
+from pprint import pprint
+
+
+def test_results():
+    cli = ConnpassClient()
+    event_id = "266898"
+    data = cli.get(event_id=event_id)
+
+    assert data["results_returned"] == 1
+
+
+cli = ConnpassClient()
+series_id = "5944"
+data = cli.get(series_id=series_id)
+
+pprint(data.keys())
